901a4ad4cd0877f46f253c7b70e62c4702ee505a
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-09-12 11:04:04 +0330

    add player logic to get metadata from url

--- src/features/player/components/Player.tsx
+++ src/features/player/components/Player.tsx
@@ -1,3 +1,4 @@
+import { parseWebStream } from "music-metadata";
 import { Cover } from "@/features/player/components/Cover";
 import { Info } from "@/features/player/components/Info";
 import { TimeIndicator } from "@/features/player/components/TimeIndicator";
@@ -6,10 +7,55 @@ import { usePlayerStore } from "@/features/player/store/player-store";
 
 const Player = () => {
   const currentTrack = usePlayerStore((state) => state.currentTrack);
+  const setCurrentTrack = usePlayerStore((state) => state.setCurrentTrack);
 
   return (
     <section>
-      <audio src={currentTrack} controls></audio>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const audio = new FormData(event.currentTarget)
+            .get("audio")
+            ?.toString();
+          if (!audio) return;
+
+          (async () => {
+            try {
+              // Fetch the audio file
+              const response = await fetch(audio);
+
+              if (!response.ok || !response.body) {
+                throw new Error(
+                  `Failed to fetch audio: ${response.statusText}`
+                );
+              }
+
+              // Extract headers for better parsing
+              const mimeType =
+                response.headers.get("content-type") || undefined;
+              const size =
+                parseInt(response.headers.get("content-length") || "0", 10) ||
+                undefined;
+
+              // Parse metadata from the web stream
+              const metadata = await parseWebStream(response.body, {
+                mimeType,
+                size,
+              });
+
+              console.log(metadata);
+            } catch (error) {
+              console.error("Error reading metadata:", error);
+            }
+          })();
+
+          setCurrentTrack(audio);
+        }}
+      >
+        <input type="url" name="audio" />
+        <button>add</button>
+      </form>
+      {currentTrack ? <audio src={currentTrack} controls></audio> : null}
       <Cover />
       <Info />
       <TimeIndicator />
--- src/features/player/store/player-store.ts
+++ src/features/player/store/player-store.ts
@@ -1,18 +1,17 @@
 import { create } from "zustand";
 
 type State = {
-  currentTrack: string;
+  currentTrack: null | string;
 };
 
 type Action = {
-  setCurrentTrack: () => void;
+  setCurrentTrack: (src: string) => void;
 };
 
 const usePlayerStore = create<State & Action>((set) => ({
-  currentTrack:
-    "https://dls.musics-fa.com/song/alibz/dlswm/Mohsen%20Chavoshi%20-%20Saate%20Divari%20(320).mp3",
-  setCurrentTrack() {
-    set({ currentTrack: "" });
+  currentTrack: null,
+  setCurrentTrack(src) {
+    set({ currentTrack: src });
   },
 }));
 
